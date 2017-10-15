61ffc5df980418b8524b241305938a337821fbf5
AndrewWaller <awalldrew2@gmail.com>
2017-10-15 11:28:51 -0700

    fixed add button, everything is on track, just need to make edit and delete functions work

--- js/scripts.js
+++ js/scripts.js
@@ -2,31 +2,15 @@ function newTodoItem() {
   let li = document.createElement("li");
   let inputValue = document.getElementById("input").value;
   let lol = document.createTextNode(inputValue);
-  li.innerHTML = '<p class="pTag"></p> <button class="addBtn">Edit</button> <button class="addBtn">Delete</button>'
-//  li.appendChild(lol);
+  li.innerHTML = '<p class="pTag"></p> <button onclick="editTodoItem()" class="addBtn">Edit</button> <button onclick="deleteTodoItem()" class="addBtn">Delete</button>'
+  li.getElementsByClassName("pTag")[0].appendChild(lol);
   document.getElementById("list").appendChild(li);
-  document.getElementsByClassName("pTag").appendChild(lol);
 };
 
+function deleteTodoItem() {
+  console.log('delete');
+};
 
-
-
-
-
-
-
-
-
-
-/*function newTodoItem() {
-  let li = document.createElement("li");
-  let inputValue = document.getElementById("input").value;
-  let lol = document.createTextNode(inputValue);
-  let editButton = document.createElement('button');
-  let deleteButton =  document.createElement('button');
-  li.appendChild(lol);
-  document.getElementById("list").appendChild(li);
-  document.getElementById("input").value = "";
-  li.append(editButton);
-  li.append(deleteButton);
-};*/
+function editTodoItem() {
+  console.log('edit/change');
+};
