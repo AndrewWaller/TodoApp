4b84f7d4e986fa928c7cb68123adc3fdc3a3dc72
AndrewWaller <awalldrew2@gmail.com>
2017-10-21 18:45:19 -0700

    working on delete button still

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,39 @@
-var idNum = 0;
+var idNum = -1;
 var array = [];
 
-
-add.addEventListener("click", function add() {
+add.addEventListener('click', function(){
+  function Item(value, index) {
+      this.value= value;
+      this.index= index;
+  }
+  idNum++;
+  let arrayItem = new Item(input.value, idNum);
+  array.push(arrayItem);
   let li = document.createElement("li");
-  li.setAttribute('id', 'liTag');
+  li.setAttribute('class', 'liTag');
+  li.setAttribute('id', idNum);
   let inputValue = document.getElementById("input").value;
   let lol = document.createTextNode(inputValue);
-  li.innerHTML = '<p class="pTag"></p> <button onclick="editTodoItem()" class="addBtn">Edit</button> <button onClick="deleteTodoItem()" class="addBtn">Delete</button>'
+  li.innerHTML = '<p class="pTag"></p> <button id="edit" onclick="editTodoItem()" class="btn">Edit</button> <button onClick="deleteTodoItem()" class="btn" id="del">Delete</button>'
   li.getElementsByClassName("pTag")[0].appendChild(lol);
   document.getElementById("list").appendChild(li);
-  idNum = idNum + 1;
-  array.push(idNum);
   console.log(array);
 });
 
 function deleteTodoItem() {
-  console.log('delete');
-  var element = document.getElementById("liTag");
-  element.outerHTML = "";
-  delete element;
-  delete array[0];
-  console.log('delete02!!!');
+  idNum--;
+  var element = document.getElementById('liTag');
+  element[0].outerHTML = "";
+  delete element['liTag'];
+  array.splice( array.indexOf('foo'), 1);
+  console.log(idNum);
   console.log(array);
+
+  /*
+var deleteTodo = function(todo) {
+            todo.parentNode.removeChild(todo);
+        };
+  */
 };
 
 function editTodoItem() {
