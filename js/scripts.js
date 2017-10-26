0b757ed0abd078e3e24ef4464fd5bb19e6390a3a
AndrewWaller <awalldrew2@gmail.com>
2017-10-25 17:49:42 -0700

    got slider to work, but only on the first li

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,16 @@
+var idNum = -1;
+var array = [];
+
+//adds new item to the list
+
 add.addEventListener('click', function(){
+    function Item(value, index) {
+        this.value= value;
+        this.index= index;
+    }
+    idNum++;
+    let arrayItem = new Item(input.value, idNum);
+    array.push(arrayItem);    
   let inputValue = document.getElementById("input").value;
   let lol = document.createTextNode(inputValue);
   if (inputValue === '') {
@@ -19,37 +31,54 @@ add.addEventListener('click', function(){
       };
   } else {
       let li = document.createElement("li");
-      li.innerHTML = '<p id="paraTag" class="pTag"></p> <button id="edit" onclick="editTodoItem(event)" class="btn">Edit</button> <button onClick="deleteTodoItem(event)" class="btn">Delete</button> <label class="switch"> <input type="checkbox"> <span class="slider round"></span> </label>'
+      li.setAttribute("class", "li01");
+      li.innerHTML = '<p id="paraTag" class="pTag"></p> <button id="edit" onclick="editTodoItem(event)" class="btn">Edit</button> <button onClick="deleteTodoItem(event)" class="btn">Delete</button> <label class="switch" onclick="checkOffTodoItem(event)"> <input type="checkbox"> <span class="slider round"></span> </label>'
       li.getElementsByClassName("pTag")[0].appendChild(lol);
       document.getElementById("list").appendChild(li);
-  }
+  };
   document.getElementById('input').value='';
 });
 
+// delete button
+
 function deleteTodoItem(event) {
-  var node = event.target.parentNode;
-  node.parentNode.removeChild(node);
+    let node = event.target.parentNode;
+    node.parentNode.removeChild(node);
 };
+
+// edit button
 
 function editTodoItem(event) {
-  let inputValue = document.getElementById("input").value;
-  let newInputValue = prompt("Change List Item:", inputValue);
-  if (inputValue == newInputValue) {
-    alert('No change were made');
-  } else {
-      var node = event.target.parentNode;
-      node.innerHTML = '<p id="paraTag" class="pTag"> ' + newInputValue + ' </p> <button id="edit" onclick="editTodoItem(event)" class="btn">Edit</button> <button onClick="deleteTodoItem(event)" class="btn" id="idNum">Delete</button>';
-  };
+    let inputValue = document.getElementById("input").value;
+    let newInputValue = prompt("Change List Item:", inputValue);
+    if (newInputValue == inputValue) {
+      alert('No change were made');
+    } else {
+      let node = event.target.parentNode;
+      node.innerHTML = '<p id="paraTag" class="pTag"> ' + newInputValue + '</p> <button id="edit" onclick="editTodoItem(event)" class="btn">Edit</button> <button onClick="deleteTodoItem(event)" class="btn">Delete</button> <label class="switch"> <input type="checkbox" onclick="checkOffTodoItem(event)"> <span class="slider round"></span> </label>';
+    };
 };
 
-document.getElementById("input")
-.addEventListener("keyup", function(event) {
+// makes slider button check off todo item
+
+function checkOffTodoItem(event) {
+    let node = event.target.parentNode;
+    let parent = node.parentNode;
+    if (parent.classList == "li01") {
+        parent.classList.remove("li01");
+        parent.classList.add("li02");
+    } else if (parent.classList == "li02") {
+        parent.classList.remove("li02");
+        parent.classList.add("li01");
+    }
+};
+
+//  allows enter button on keyboard to submit new task
+
+document.getElementById("input").addEventListener("keyup", function(event) {
 event.preventDefault();
 if (event.keyCode == 13) {
     document.getElementById("add").click();
 }
 });
 
-var bg_colour = Math.floor(Math.random() * 16777215).toString(16);
-bg_colour = "#" + ("000000" + bg_colour).slice(-6);
-document.bgColor = bg_colour;
